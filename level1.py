aa4db254ca52462958fe358e746d430b031eda7c
holdenh <holden.halferty@okstate.edu>
2019-06-27 03:23:31 -0500

    added the output needed to get to level2.

--- level1.py
+++ level1.py
@@ -2,6 +2,7 @@ import string
 
 # take the given string
 sourceString = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
+weburlNeeded = 'map'
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 specials = ["'", " ", "(", ")", "."]
 #hints given
@@ -25,3 +26,18 @@ for char in sourceString :
         outputstring +=(alphabet[newLocation])
 #print the final output.
 print(outputstring)
+newURL =''
+for char in weburlNeeded :
+    if char in specials :
+        newURL +=(char)
+        continue
+    #do the shift.
+    newLocation = alphabet.index(char) + 2
+    #check if new location needs to be wrapped to beginning.
+    if newLocation > len(alphabet)-1 :
+        #have to subtract by one again to account for zero based indexing.
+        rollback = (newLocation - (len(alphabet)-1)) - 1
+        newURL +=(alphabet[rollback])
+    else :
+        newURL +=(alphabet[newLocation])
+print("\nThis is the newurl needed in front of .html: " + newURL)
